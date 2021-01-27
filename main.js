1af4f224cf6e844c47b77010daa6fb77c51daf82
Dani Crosby <dani.crosby1111@gmail.com>
2021-01-27 13:53:42 -0600

    setup files, structured html and css page

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
 console.log("Greetings beautiful from main.js")
+
+// PROJECTS ARRAY
+const projects = [
+  {
+    title: "Cool Project", 
+    screenshot: "http://gotoflashgames.com/files/file/033.jpg", 
+    description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
+    technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+    available: true,
+    url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+    githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
+  }
+];
+
+
+//PRINT TO DOM FUNCTION
+const printToDom = (arg1, arg2) => {
+
+};
+
+//CREATE PROJECT CARDS FUNCTION
+const createProjectCards = (projects) => {
+  for (let i = 0; i < projects.length; i++ ) {
+
+  }
+}
+
+//CALL THE CREATE PROJECTS FUNCTION BELOW
+// createProjectCards();
