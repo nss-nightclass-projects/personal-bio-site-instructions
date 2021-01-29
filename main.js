db70a22dc2ce187ca43890dc874fbabce5568f29
Dani Crosby <dani.crosby1111@gmail.com>
2021-01-28 23:12:15 -0600

    started Project section, need to refactor domString

--- main.js
+++ main.js
@@ -14,17 +14,53 @@ const projects = [
 ];
 
 
-//PRINT TO DOM FUNCTION
-const printToDom = (arg1, arg2) => {
+const printToDom = (divId, textToPrint) => {
+  const selectedDiv = document.querySelector(divId);
+  selectedDiv.innerHTML = textToPrint;
+}
 
-};
 
 //CREATE PROJECT CARDS FUNCTION
-const createProjectCards = (projects) => {
-  for (let i = 0; i < projects.length; i++ ) {
-
+const createProjectCards = (arr) => {
+  let domString = '';
+  for (let i = 0; i < arr.length; i++) {
+    domString += `<div class="card my-2" style="width: 18rem;" id=${i}>
+                    <div class="img-container" style="background-image: url('${arr[i].imageUrl}');"></div>
+                    <div class="card-body">
+                      <p class="card-text">${arr[i].title}</p>
+                      <p class="card-text">${arr[i].screenshot}</p>
+                      <p class="card-text">${arr[i].description}</p>
+                      <p class="card-text">${arr[i].technologiesUsed}</p>
+                      <p class="card-text">${arr[i].available}</p>
+                      <p class="card-text">${arr[i].url}</p>
+                      <p class="card-text">${arr[i].githubUrl}</p>
+                      <button type="button" class="btn btn-danger" id="${i}">Delete</button>
+                    </div>
+                  </div>`;
   }
+
+  printToDom('#projects', domString);
 }
+
+const handleButtonClick = (e) => {
+  const buttonId = e.target.id;
+
+
+  const selectedProjects = [];
+  for (let i = 0; i < projects.length; i++) {
+    if (projects[i].available === buttonId) {
+      selectedProjects.push(projects[i]);
+    }
+  }
+
+}
+
 
 //CALL THE CREATE PROJECTS FUNCTION BELOW
 // createProjectCards();
+
+const init = () => {
+  createProjectCards(projects);
+}
+
+init();
