519e6ead2deed1c66899853978cefb7ecfebf515
Dani Crosby <dani.crosby1111@gmail.com>
2021-01-29 23:12:01 -0600

    added project page JS card functionality

--- main.js
+++ main.js
@@ -1,30 +1,27 @@
-console.log("Greetings beautiful from main.js")
+console.log("Greetings beautiful from main.js");
 
-// PROJECTS ARRAY
 const projects = [
   {
-    title: "Cool Project", 
-    screenshot: "http://gotoflashgames.com/files/file/033.jpg", 
-    description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
-    technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+    title: "Project Name",
+    screenshot: "http://gotoflashgames.com/files/file/033.jpg",
+    description: "Description", 
+    technologiesUsed:"HTML, CSS, Vanilla JavaScript, Version Control with Github",
     available: true,
-    url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
-    githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
-  }
+    url: "https://github.com/nss-evening-cohort-8/js-part-deux", 
+    githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux",
+  },
 ];
-
 
 const printToDom = (divId, textToPrint) => {
   const selectedDiv = document.querySelector(divId);
   selectedDiv.innerHTML = textToPrint;
-}
+};
 
-
-//CREATE PROJECT CARDS FUNCTION
 const createProjectCards = (arr) => {
-  let domString = '';
+  let domString = "";
   for (let i = 0; i < arr.length; i++) {
-    domString += `<div class="card my-2" style="width: 18rem;" id=${i}>
+    if (arr[i].available === true) {
+      domString += `<div class="card my-2" style="width: 18rem;" id=${i}>
                     <div class="img-container" style="background-image: url('${arr[i].imageUrl}');"></div>
                     <div class="card-body">
                       <p class="card-text">${arr[i].title}</p>
@@ -34,33 +31,16 @@ const createProjectCards = (arr) => {
                       <p class="card-text">${arr[i].available}</p>
                       <p class="card-text">${arr[i].url}</p>
                       <p class="card-text">${arr[i].githubUrl}</p>
-                      <button type="button" class="btn btn-danger" id="${i}">Delete</button>
                     </div>
                   </div>`;
-  }
-
-  printToDom('#projects', domString);
-}
-
-const handleButtonClick = (e) => {
-  const buttonId = e.target.id;
-
-
-  const selectedProjects = [];
-  for (let i = 0; i < projects.length; i++) {
-    if (projects[i].available === buttonId) {
-      selectedProjects.push(projects[i]);
     }
   }
 
-}
-
-
-//CALL THE CREATE PROJECTS FUNCTION BELOW
-// createProjectCards();
+  printToDom("#projectsPage", domString);
+};
 
 const init = () => {
   createProjectCards(projects);
-}
+};
 
 init();
