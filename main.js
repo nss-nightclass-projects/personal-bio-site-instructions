f39e1873f0fd39cb17af4b6357ff1d5f431df345
Ben Jarrett <b.cl.jarrett@gmail.com>
2021-01-31 16:26:04 -0600

    Created print to dom functionality

--- main.js
+++ main.js
@@ -137,3 +137,37 @@ const projects = [
     githubUrl: "https://github.com/BenJarrett/personal-bio-site-instructions"
   }
 ]
+
+const PrintToDom = (divId, printThisText) => {
+  const selectThisDiv = document.querySelector(divId)
+  selectThisDiv.innerHTML = printThisText;
+}
+
+const createProjectCards = (taco) => {
+  let domString = '';
+
+  for (let i = 0; i < taco.length; i++) {
+    domString += `<div class="card my-2" style="width: 18rem;" id=${i}>
+    <div class="img-container" style="background-image: url('${taco[i].imageURL}');"></div>
+    <div class="card-body">
+      <p class="card-text">${taco[i].title}</p>
+      <p class="card-text">${taco[i].screenshot}</p>
+      <p class="card-text">${taco[i].description}</p>
+      <p class="card-text">${taco[i].technologiesUsed}</p>
+      <p class="card-text">${taco[i].available}</p>
+      <p class="card-text">${taco[i].url}</p>
+      <p class="card-text">${taco[i].githubUrl}</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>`
+  }
+  PrintToDom('#projectsPage', domString);
+
+}
+
+const init = () => {
+  createProjectCards(projects);
+}
+
+
+init()
