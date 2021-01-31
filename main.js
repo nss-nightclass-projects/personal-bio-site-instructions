a1649c0defbb0d1131cc94e937d6de29279d6792
Ben Jarrett <b.cl.jarrett@gmail.com>
2021-01-30 21:52:00 -0600

    Created an array in objects.

--- main.js
+++ main.js
@@ -1,1 +1,139 @@
-console.log("Personal Bio Baby")
+// console.log("Personal Bio Baby")
+
+const projects = [
+  {
+    title: "First Project",
+    screenshot: "http://gotoflashgames.com/files/file/033.jpg",
+    description: "First project of many!",
+    technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+    available: true,
+    url: "https://github.com/BenJarrett/personal-bio-site-instructions",
+    githubUrl: "https://github.com/BenJarrett/personal-bio-site-instructions"
+  },
+  {
+    title: "First Project",
+    screenshot: "http://gotoflashgames.com/files/file/033.jpg",
+    description: "First project of many!",
+    technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+    available: true,
+    url: "https://github.com/BenJarrett/personal-bio-site-instructions",
+    githubUrl: "https://github.com/BenJarrett/personal-bio-site-instructions"
+  },
+  {
+    title: "First Project",
+    screenshot: "http://gotoflashgames.com/files/file/033.jpg",
+    description: "First project of many!",
+    technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+    available: true,
+    url: "https://github.com/BenJarrett/personal-bio-site-instructions",
+    githubUrl: "https://github.com/BenJarrett/personal-bio-site-instructions"
+  },
+  {
+    title: "First Project",
+    screenshot: "http://gotoflashgames.com/files/file/033.jpg",
+    description: "First project of many!",
+    technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+    available: true,
+    url: "https://github.com/BenJarrett/personal-bio-site-instructions",
+    githubUrl: "https://github.com/BenJarrett/personal-bio-site-instructions"
+  },
+  {
+    title: "First Project",
+    screenshot: "http://gotoflashgames.com/files/file/033.jpg",
+    description: "First project of many!",
+    technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+    available: true,
+    url: "https://github.com/BenJarrett/personal-bio-site-instructions",
+    githubUrl: "https://github.com/BenJarrett/personal-bio-site-instructions"
+  },
+  {
+    title: "First Project",
+    screenshot: "http://gotoflashgames.com/files/file/033.jpg",
+    description: "First project of many!",
+    technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+    available: true,
+    url: "https://github.com/BenJarrett/personal-bio-site-instructions",
+    githubUrl: "https://github.com/BenJarrett/personal-bio-site-instructions"
+  },
+  {
+    title: "First Project",
+    screenshot: "http://gotoflashgames.com/files/file/033.jpg",
+    description: "First project of many!",
+    technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+    available: true,
+    url: "https://github.com/BenJarrett/personal-bio-site-instructions",
+    githubUrl: "https://github.com/BenJarrett/personal-bio-site-instructions"
+  },
+  {
+    title: "First Project",
+    screenshot: "http://gotoflashgames.com/files/file/033.jpg",
+    description: "First project of many!",
+    technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+    available: true,
+    url: "https://github.com/BenJarrett/personal-bio-site-instructions",
+    githubUrl: "https://github.com/BenJarrett/personal-bio-site-instructions"
+  },
+  {
+    title: "First Project",
+    screenshot: "http://gotoflashgames.com/files/file/033.jpg",
+    description: "First project of many!",
+    technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+    available: true,
+    url: "https://github.com/BenJarrett/personal-bio-site-instructions",
+    githubUrl: "https://github.com/BenJarrett/personal-bio-site-instructions"
+  },
+  {
+    title: "First Project",
+    screenshot: "http://gotoflashgames.com/files/file/033.jpg",
+    description: "First project of many!",
+    technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+    available: true,
+    url: "https://github.com/BenJarrett/personal-bio-site-instructions",
+    githubUrl: "https://github.com/BenJarrett/personal-bio-site-instructions"
+  },
+  {
+    title: "First Project",
+    screenshot: "http://gotoflashgames.com/files/file/033.jpg",
+    description: "First project of many!",
+    technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+    available: true,
+    url: "https://github.com/BenJarrett/personal-bio-site-instructions",
+    githubUrl: "https://github.com/BenJarrett/personal-bio-site-instructions"
+  },
+  {
+    title: "First Project",
+    screenshot: "http://gotoflashgames.com/files/file/033.jpg",
+    description: "First project of many!",
+    technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+    available: true,
+    url: "https://github.com/BenJarrett/personal-bio-site-instructions",
+    githubUrl: "https://github.com/BenJarrett/personal-bio-site-instructions"
+  },
+  {
+    title: "First Project",
+    screenshot: "http://gotoflashgames.com/files/file/033.jpg",
+    description: "First project of many!",
+    technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+    available: true,
+    url: "https://github.com/BenJarrett/personal-bio-site-instructions",
+    githubUrl: "https://github.com/BenJarrett/personal-bio-site-instructions"
+  },
+  {
+    title: "First Project",
+    screenshot: "http://gotoflashgames.com/files/file/033.jpg",
+    description: "First project of many!",
+    technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+    available: true,
+    url: "https://github.com/BenJarrett/personal-bio-site-instructions",
+    githubUrl: "https://github.com/BenJarrett/personal-bio-site-instructions"
+  },
+  {
+    title: "First Project",
+    screenshot: "http://gotoflashgames.com/files/file/033.jpg",
+    description: "First project of many!",
+    technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+    available: true,
+    url: "https://github.com/BenJarrett/personal-bio-site-instructions",
+    githubUrl: "https://github.com/BenJarrett/personal-bio-site-instructions"
+  }
+]
