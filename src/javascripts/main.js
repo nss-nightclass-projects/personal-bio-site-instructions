9cada97b65f32bcfb682473d3cc4aa52625585a5
Dani Crosby <dani.crosby1111@gmail.com>
2021-03-05 10:29:40 -0600

    refactored project with webpack, made nessesary changed to index, scss

---
+++ src/javascripts/main.js
@@ -0,0 +1,15 @@
+// USE WITH FIREBASE AUTH
+// import checkLoginStatus from './helpers/auth';
+import 'bootstrap'; // import bootstrap elements and js
+
+import '../styles/main.scss';
+import initialScreen from './initialScreen';
+
+const init = () => {
+  initialScreen();
+
+  // USE WITH FIREBASE AUTH
+  // checkLoginStatus();
+};
+
+init();
---
+++ src/javascripts/techPage.js
@@ -0,0 +1,13 @@
+import renderToDom from './helpers/renderToDom';
+
+const techPage = () => {
+  let domString = '';
+  domString += `
+
+  
+  `;
+
+  renderToDom('tech-page', domString);
+};
+
+export default techPage;
